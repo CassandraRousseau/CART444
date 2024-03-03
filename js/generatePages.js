609f33732ad1de82af5210079ba9daf11cf980c8
Cassandra Rousseau <cass201001@gmail.com>
2024-03-03 16:43:17 -0500

    fixing urls

--- js/generatePages.js
+++ js/generatePages.js
@@ -1,13 +1,13 @@
 // Array of URLs
 var urls = [
-    "Pages/animation.html",
-    "Pages/dance.html",
-    "Pages/design.html",
-    "Pages/drawing.html",
-    "Pages/game-development.html",
-    "Pages/painting.html",
-    "Pages/photography.html",
-    "Pages/sculpting.html"
+    "../CART444/Pages/animation.html",
+    "../CART444/Pages/dance.html",
+    "../CART444/Pages/design.html",
+    "../CART444/Pages/drawing.html",
+    "../CART444/Pages/game-development.html",
+    "../CART444/Pages/painting.html",
+    "../CART444/Pages/photography.html",
+    "../CART444/Pages/sculpting.html"
     // Add more URLs as needed
 ];
 // Function to generate a random URL and redirect to it
